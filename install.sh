328a38a7acb9fe2957a8bb535c71ce0004218125
Yann Salaün <yannsalaun1@gmail.com>
2016-10-06 01:10:36 +0200

    Remove rust plugin

--- install.sh
+++ install.sh
@@ -9,4 +9,3 @@ mkdir -p ~/.vim/bundle && cd ~/.vim/bundle
 git clone https://github.com/tpope/vim-sensible
 git clone https://github.com/fatih/vim-go
 git clone https://github.com/rhysd/vim-clang-format
-git clone https://github.com/rust-lang/rust.vim
--- vimrc
+++ vimrc
@@ -37,6 +37,3 @@ let g:go_highlight_types = 1
 let g:go_highlight_operators = 1
 let g:go_highlight_build_constraints = 1
 let g:go_fmt_command = "goimports"
-
-" rust.vim
-let g:rustfmt_autosave = 1
