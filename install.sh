b815a0e3a5d76c499f5779c680ace71442505505
Yann Salaün <yannsalaun1@gmail.com>
2017-01-23 18:16:22 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
-#!/bin/sh -ex
+#!/bin/sh -eu
 
-DIR=$(dirname "$0")
-cp "$DIR"/vimrc ~/.vimrc
+dotvim_dir=$(dirname "$0")
+ln -fs "$dotvim_dir/vimrc" ~/.vimrc
 
-rm -fr ~/.vim
-git clone https://github.com/tpope/vim-pathogen ~/.vim
-mkdir -p ~/.vim/bundle && cd ~/.vim/bundle
-git clone https://github.com/tpope/vim-sensible
-git clone https://github.com/fatih/vim-go
-git clone https://github.com/rhysd/vim-clang-format
+checkout() {
+	repo=$1
+	path=$2
+	if [ ! -d "$path/.git" ]
+	then
+		git clone -q "$repo" "$path"
+	else
+		(cd "$path"; git fetch && git reset -q --hard)
+	fi
+}
+
+checkout https://github.com/tpope/vim-pathogen ~/.vim
+checkout https://github.com/tpope/vim-sensible ~/.vim/bundle/vim-sensible
+checkout https://github.com/fatih/vim-go ~/.vim/bundle/vim-go
+checkout https://github.com/rhysd/vim-clang-format ~/.vim/bundle/vim-clang-format
