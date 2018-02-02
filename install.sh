49df49b5c26d094fe38bfc6eac5a3e1c11b07db9
Yann Salaün <yannsalaun1@gmail.com>
2018-02-02 21:52:50 +0100

    Remove vim-clang-format plugin

--- install.sh
+++ install.sh
@@ -17,4 +17,3 @@ checkout() {
 checkout https://github.com/tpope/vim-pathogen ~/.vim
 checkout https://github.com/tpope/vim-sensible ~/.vim/bundle/vim-sensible
 checkout https://github.com/fatih/vim-go ~/.vim/bundle/vim-go
-checkout https://github.com/rhysd/vim-clang-format ~/.vim/bundle/vim-clang-format
