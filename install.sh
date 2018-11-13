8e6b4b9516f2a6f18d65c6c8947a4f0dbb3d45a8
Yann Salaün <yann.salaun@ulule.com>
2018-11-13 22:12:45 +0100

    checkout branches

--- install.sh
+++ install.sh
@@ -6,14 +6,16 @@ ln -fs "$dotvimdir/vimrc" ~/.vimrc
 checkout() {
 	repo=$1
 	path=$2
+	branch=$3
 	if [ ! -d "$path/.git" ]
 	then
 		git clone -q "$repo" "$path"
 	else
-		git -C "$path" pull -q
+		git -C "$path" fetch -q
 	fi
+	git -C "$path" checkout -q "$branch"
 }
 
-checkout https://github.com/tpope/vim-pathogen ~/.vim
-checkout https://github.com/tpope/vim-sensible ~/.vim/bundle/vim-sensible
-checkout https://github.com/fatih/vim-go ~/.vim/bundle/vim-go
+checkout https://github.com/tpope/vim-pathogen ~/.vim master
+checkout https://github.com/tpope/vim-sensible ~/.vim/bundle/vim-sensible master
+checkout https://github.com/fatih/vim-go ~/.vim/bundle/vim-go v1.19
